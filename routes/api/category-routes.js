fb48efab8baf81387d78d31e081da4cfbaba0d6e
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-02-23 19:08:34 -0500

    finished all the get commands in all the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,25 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+//Find all categories, including its associated Products 
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+//Find one category by its `id` value, including its associated Products 
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk({
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+
+//Create a new category 
+router.post('/', async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
