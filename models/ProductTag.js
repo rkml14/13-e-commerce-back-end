b2ee9fcd7c95e5792515c95f491ac89f60fd07e2
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-02-23 19:24:15 -0500

    corrected spelling mistake in route

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -19,7 +19,7 @@ ProductTag.init(
         key: 'id',
       },
     },
-    product_id: {
+    tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
--- models/index.js
+++ models/index.js
@@ -4,24 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
-// Categories have many Products
+// Category have many Product
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Product belongToMany Tag (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "tag_id",
 });
 
-// Tags belongToMany Products (through ProductTag)
+// Tag belongToMany Product (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: "product_id",
